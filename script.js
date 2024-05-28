cfa1825cd0881c4362e6e402117de0c97ae9d7db
gabriel-05-dev <g.luchita@protonmail.com>
2024-05-28 14:32:45 +0100

    Added creategrid function, added a clear grid function, added value checking for security, added eventlisterners and css styling (basic)

--- script.js
+++ script.js
@@ -3,18 +3,39 @@ const Button16 = document.querySelector("#b16");
 const Button32 = document.querySelector("#b32");
 const Button64 = document.querySelector("#b64");
 const Button128 = document.querySelector("#b128");
+const clearBtn = document.querySelector('#clear')
 
-Button16.addEventListener('click', createGrid(16));
-Button32.addEventListener('click', () => {});
-Button64.addEventListener('click', () => {});
-Button128.addEventListener('click', () => {});
+
+Button16.addEventListener('click', () => {clearGrid(),createGrid(25)});
+Button32.addEventListener('click', () => {clearGrid(),createGrid(35)});
+Button64.addEventListener('click', () => {clearGrid(),createGrid(45)});
+Button128.addEventListener('click', () => {clearGrid(),createGrid(60)});
+clearBtn.addEventListener('click', () => {clearGrid()});
 
 function createGrid(amount) {
-    for (let i = 0; i <= amount; i++) {
-        const newdiv = document.createElement("div");
-        containerDiv.appendChild(newdiv)};
+    if (amount == 25) {
+        containerDiv.style = 'max-width: 25%;'
+        clearGrid(),createBoard(amount);
+    } else if (amount == 35) {
+        containerDiv.style = 'max-width: 35%;'
+        clearGrid(),createBoard(amount);
+    } else if (amount == 45) {
+        containerDiv.style = 'max-width: 45%;'
+        clearGrid(),createBoard(amount);
+    } else if (amount == 60) {
+        containerDiv.style = 'max-width: 65%;'
+        clearGrid(),createBoard(amount);
+    };
+
+    function createBoard(amount) {
+        for (let i = 0; i <= (amount*amount); i++) {
+            const newdiv = document.createElement("div");
+            newdiv.classList = ('paint');
+            containerDiv.appendChild(newdiv)};
+        };
 };
 
-function clearSpace() {
-    const containerDiv = //todo
+
+function clearGrid() {
+    containerDiv.innerHTML='';
 };
